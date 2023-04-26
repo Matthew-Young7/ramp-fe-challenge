6eaf352c5908d5e2dee6f7f113a0a405073bdcd2
Matthew Young <y.matt12@gmail.com>
2023-04-25 17:15:35 -0700

    Initial commit

---
+++ src/components/InputCheckbox/index.tsx
@@ -0,0 +1,35 @@
+import classNames from "classnames"
+import { useRef, useState } from "react"
+import { InputCheckboxComponent } from "./types"
+
+export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+  const [isChecked, setIsChecked] = useState(checked)
+
+  const handleCheck = () => {
+    const changed = !isChecked
+    setIsChecked(changed)
+    onChange(changed)
+    console.log("handleToggle called")
+  }
+
+  return (
+    <div className="RampInputCheckbox--container" data-testid={inputId}>
+      <label
+        className={classNames("RampInputCheckbox--label", {
+          "RampInputCheckbox--label-checked": checked,
+          "RampInputCheckbox--label-disabled": disabled,
+        })}
+        onClick={handleCheck}
+      />
+      <input
+        id={inputId}
+        type="checkbox"
+        className="RampInputCheckbox--input"
+        checked={checked}
+        disabled={disabled}
+        onChange={() => {}}
+      />
+    </div>
+  )
+}
